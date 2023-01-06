9619efeb834bf01ac38c3c71c8bc6597eb34211e
Miguel Gueifao <miguel.gueifao@gmail.com>
2023-01-06 14:04:30 +0100

    Updated to run with newer version and converted file to unix

--- bot.py
+++ bot.py
@@ -20,60 +20,67 @@ from scripts.make_user_database import *
 import logging
 from dotenv import load_dotenv
 import discord
-import os, sys
+import os
 from discord.ext import commands
-import logging_setup
+import dropi
 
 load_dotenv()
 
 # Define environments in dictionary to allow for scaling
 environments = {
-    'PROD': {
-        'Branch': 'Production',
-        'Token': 'TOKEN_PROD'},
-    'DEV': {
-        'Branch': 'Development',
-        'Token': 'TOKEN_DEV',}}
+	'PROD': {
+		'Branch': 'Production',
+		'Token': 'TOKEN_PROD'},
+	'DEV': {
+		'Branch': 'Development',
+		'Token': 'TOKEN_DEV',}}
 
 current_env = os.getenv('ENV')
 # Verify that the provided argument is a working environment
 # Log the environment being used and grab the matching token
 if current_env in list(environments.keys()):
-    logging.info(f"Running on {environments[current_env]['Branch']}")
-    token = os.environ.get(environments[current_env]['Token'])
+	logging.info(f"Running on {environments[current_env]['Branch']}")
+	token = os.environ.get(environments[current_env]['Token'])
 else:
-    logging.error(f"Unrecognized environment: {current_env}")
-    exit()
+	logging.error(f"Unrecognized environment: {current_env}")
+	exit()
 
 # defines the appropriate id's for each environment
 if current_env == 'PROD':
-    import ids_prod as ids
+	import ids_prod as ids
 else:
-    import ids_dev as ids
+	import ids_dev as ids
 
 # Gives the bots additionnal permissions
 # Defines the prefix for commands
 intents = discord.Intents().all()
-client = commands.Bot(command_prefix = '.', intents=intents)
+
+class Bot42(commands.Bot):
+	async def load_extensions(self):
+		for filename in os.listdir('./cogs'):
+			if filename.endswith('.py'):
+				await client.load_extension(f'cogs.{filename[:-3]}')
+
+	async def setup_hook(self) -> None:
+		await self.load_extensions()
+
+
+client = Bot42(command_prefix = '.', intents=intents)
 
 @client.command()
 async def load(ctx, extension):
-    client.load_extension(f'cogs.{extension}')
+	await client.load_extension(f'cogs.{extension}')
 
 @client.command()
 async def unload(ctx, extension):
-    if('staff' in [x.name for x in ctx.author.roles]):
-        client.unload_extension(f'cogs.{extension}')
+	if('staff' in [x.name for x in ctx.author.roles]):
+		await client.unload_extension(f'cogs.{extension}')
 
 @client.command()
 async def reload(ctx, extension):
-    client.unload_extension(f'cogs.{extension}')
-    client.load_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        client.load_extension(f'cogs.{filename[:-3]}')
+	await client.unload_extension(f'cogs.{extension}')
+	await client.load_extension(f'cogs.{extension}')
 
 if not os.path.exists('./users_id_database.json'):
-    create_user_database()
+	create_user_database()
 client.run(token)
